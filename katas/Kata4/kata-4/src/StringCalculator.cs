4ff593ff97649c7d22a7a2dca3a0d3c7e660faca
Santiago Rodriguez <santinacional123.sr@gmail.com>
2025-12-09 16:06:30 -0500

    🔵Calculate_NumbersWithCustomDelimiter_ReturnsSum

--- katas/Kata4/kata-4/src/StringCalculator.cs
+++ katas/Kata4/kata-4/src/StringCalculator.cs
@@ -4,18 +4,18 @@ namespace Kata4
     {
         public static int Calculate(string input)
         {
-            if (input.StartsWith("//"))
+            if (HasDelimiter(input))
             {
                 var endDelimiter = input.IndexOf('\n');
-                var delimiter = input.Substring(2, endDelimiter - 2);
-                var extractedNumbers = input.Substring(endDelimiter + 1);
-                var numbers = extractedNumbers.Split(delimiter);
+                var extractedNumbers = GetNumbersWithDelimiter(input, endDelimiter);
+                var delimiter = GetDelimiter(input, endDelimiter);
+                var numbers = ExtractValues(extractedNumbers, delimiter);
                 return numbers.Sum(int.Parse);
             }
 
             input = CleanNewLines(input);
 
-            var values = ExtractValues(input);
+            var values = ExtractValues(input, ",");
             if (IsEmptyString(input))
                 return 0;
             var sum = values.Length == 1 ? int.Parse(input) : 0;
@@ -25,7 +25,16 @@ namespace Kata4
             return sum;
         }
 
-        private static string[] ExtractValues(string input) => input.Split(',');
+        private static string GetNumbersWithDelimiter(string input, int endDelimiter)
+        {
+            return input.Substring(endDelimiter + 1);
+        }
+
+        private static string GetDelimiter(string input, int endDelimiter) => input[2..endDelimiter];
+
+        private static bool HasDelimiter(string input) => input.StartsWith("//");
+
+        private static string[] ExtractValues(string input, string delimiter) => input.Split(delimiter);
 
         private static string CleanNewLines(string input)
         {
