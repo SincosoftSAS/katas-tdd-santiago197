8f4e747a17bd80aa8e91536e1268ae745f11154c
Santiago Rodriguez <santinacional123.sr@gmail.com>
2025-12-09 12:37:37 -0500

    🟢Calculate_TwoNumbers_ReturnsSum

--- katas/Kata4/kata-4/src/StringCalculator.cs
+++ katas/Kata4/kata-4/src/StringCalculator.cs
@@ -4,8 +4,11 @@ namespace Kata4
     {
         public static int Calculate(string input)
         {
+            var values = input.Split(',');
             if (input == "")
                 return 0;
+            if (values.Length > 1)
+                return int.Parse(values[0]) + int.Parse(values[1]);
             return int.Parse(input);
         }
     }
