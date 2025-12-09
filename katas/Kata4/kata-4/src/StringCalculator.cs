1da587ed21e2a215fe634d6de2ef01b2650b76a4
Santiago Rodriguez <santinacional123.sr@gmail.com>
2025-12-09 14:24:21 -0500

    🔵Calculate_NumbersWithLines_ReturnsSum

--- katas/Kata4/kata-4/src/StringCalculator.cs
+++ katas/Kata4/kata-4/src/StringCalculator.cs
@@ -4,8 +4,8 @@ namespace Kata4
     {
         public static int Calculate(string input)
         {
-            input = input.Replace("\n", ",");
-            var values = input.Split(',');
+            input = CleanNewLines(input);
+            var values = ExtractValues(input);
             if (IsEmptyString(input))
                 return 0;
             var sum = values.Length == 1 ? int.Parse(input) : 0;
@@ -13,6 +13,14 @@ namespace Kata4
             if (values.Length > 1) sum += values.Sum(int.Parse);
 
             return sum;
+        }
+
+        private static string[] ExtractValues(string input) => input.Split(',');
+
+        private static string CleanNewLines(string input)
+        {
+            input = input.Replace("\n", ",");
+            return input;
         }
 
         private static bool IsEmptyString(string input) => input == "";
