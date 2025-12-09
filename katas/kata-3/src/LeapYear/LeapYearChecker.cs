0c810213129d70ff0f5c59a026a51902a14883a3
Santiago Rodriguez <santinacional123.sr@gmail.com>
2025-12-09 09:57:35 -0500

    🔴🟢🔵

--- katas/kata-3/src/LeapYear/LeapYearChecker.cs
+++ katas/kata-3/src/LeapYear/LeapYearChecker.cs
@@ -4,8 +4,17 @@ namespace LeapYear
     {
         public static bool IsLeapYear(int year)
         {
-            // TODO: Implement leap year logic using TDD
-            throw new NotImplementedException("Implement using TDD: Red → Green → Refactor");
+            return !NoEsBisiesto(year) && EsBisiesto(year);
         }
+
+        private static bool EsBisiesto(int year) => EsDivisiblePor400(year) || EsDivisiblePor4(year);
+
+        private static bool NoEsBisiesto(int year) => EsDivisiblePor100(year) && !EsDivisiblePor400(year);
+
+        private static bool EsDivisiblePor100(int year) => year % 100 == 0;
+
+        private static bool EsDivisiblePor4(int year) => year % 4 == 0;
+
+        private static bool EsDivisiblePor400(int year) => year % 400 == 0;
     }
 }
